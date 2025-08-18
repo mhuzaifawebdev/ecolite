2a644155bf7dd03ec37c69252a2cf1154b34e1b5
MhuzaifaWebDev <mhuzaifawebdev@gmail.com>
2025-08-18 18:54:30 +0500

    Finally

--- src/Config/Urls.js
+++ src/Config/Urls.js
@@ -2,5 +2,5 @@
 
 // export const baseUrlImg = 'http://localhost:4000/uploads';
 
-export const baseUrl = " http://localhost:8990";
-export const baseUrlImg = " http://localhost:8990/uploads";
+export const baseUrl = "http://surveyor-backend.vercel.app";
+export const baseUrlImg = "http://surveyor-backend.vercel.app/uploads";
